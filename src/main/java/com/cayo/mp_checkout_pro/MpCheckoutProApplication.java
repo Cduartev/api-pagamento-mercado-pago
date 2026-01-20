3e03aadf084c3b548d390ff28f593a47f575e470
Cduartev <cayooduartevidal@gmail.com>
2026-01-19 23:56:03 -0300

    Api Pagamento Mercado pago para teste

---
+++ src/main/java/com/cayo/mp_checkout_pro/MpCheckoutProApplication.java
@@ -0,0 +1,13 @@
+package com.cayo.mp_checkout_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpCheckoutProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpCheckoutProApplication.class, args);
+	}
+
+}
